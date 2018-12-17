77925f067d0d927c7e3b56ddf1c9ee0daccd5c2e
ehmicky <ehmicky@gmail.com>
2018-12-17 17:00:00 +0200

    Validate options differently for validatePath.sync()

--- src/main.js
+++ src/main.js
@@ -9,49 +9,49 @@ const { statValidate } = require('./stat_validate')
 const { statNormalize } = require('./stat_normalize')
 
 // Validate and normalize a path.
+// Only checks the path string, i.e. does not check if file exists.
+const validatePathSync = function(path, opts) {
+  const optsA = getOptions({ opts, type: 'sync' })
+  const pathA = handlePath(path, optsA)
+  return pathA
+}
+
+// Validate and normalize a path.
 // Also checks if the file exists, its permissions, etc.
 const validatePath = async function(path, opts) {
-  const { opts: optsA, path: pathA } = beforeStat(path, opts)
+  const optsA = getOptions({ opts, type: 'async' })
+  const pathA = handlePath(path, optsA)
 
   if (pathA === undefined) {
     return
   }
 
-  const pathB = await afterStat(pathA, optsA)
+  const pathB = await handleStat(pathA, optsA)
   return pathB
-}
-
-// Validate and normalize a path.
-// Only checks the path string, i.e. does not check if file exists.
-const validatePathSync = function(path, opts) {
-  const { path: pathA } = beforeStat(path, opts)
-  return pathA
 }
 
 // eslint-disable-next-line fp/no-mutation
 validatePath.sync = validatePathSync
 
 // Validation/normalization that does not use `stat`
-const beforeStat = function(path, opts) {
-  const optsA = getOptions({ opts })
-
+const handlePath = function(path, opts) {
   const pathA = addDefault(path, opts)
 
   // If path was `undefined` and no `opts.defaultValue` was defined,
   // we return `undefined`
   if (pathA === undefined) {
-    return { opts: optsA }
+    return
   }
 
   const pathB = pathNormalize(pathA, opts)
 
   pathValidate(pathB, opts)
 
-  return { opts: optsA, path: pathB }
+  return pathB
 }
 
 // Validation/normalization that use `stat`
-const afterStat = async function(path, opts) {
+const handleStat = async function(path, opts) {
   const stat = await getStat(path)
 
   await statValidate(path, stat, opts)
--- src/options.js
+++ src/options.js
@@ -11,37 +11,53 @@ const {
 } = require('./path_validate')
 
 // Validate options and assign default options
-const getOptions = function({ opts = {} }) {
+// `validatePath.sync()` can only use sync options.
+// `validatePath()` can use sync or async options.
+const getOptions = function({ opts = {}, type }) {
   const optsA = normalizeFilters({ opts })
 
-  validate(optsA, { exampleConfig: EXAMPLE_OPTS, condition })
+  validate(optsA, { exampleConfig: EXAMPLE_OPTS[type], condition })
 
   const optsB = omitBy(optsA, value => value === undefined)
-  const optsC = { ...DEFAULT_OPTS, ...optsB }
+  const optsC = { ...DEFAULT_OPTS[type], ...optsB }
   return optsC
 }
 
-const DEFAULT_OPTS = {
+// Used for validation + example + defaults
+const SYNC_DEFAULT_OPTS = {
   base: getDefaultBase(),
   platform: getDefaultPlatform(),
-  followSymlinks: false,
   lowercase: false,
+}
+
+const ASYNC_DEFAULT_OPTS = {
+  ...SYNC_DEFAULT_OPTS,
+  followSymlinks: false,
   allowSpecial: false,
   canRead: true,
 }
 
-const EXAMPLE_OPTS = {
-  ...DEFAULT_OPTS,
+const DEFAULT_OPTS = { sync: SYNC_DEFAULT_OPTS, async: ASYNC_DEFAULT_OPTS }
+
+// Used for validation + example, but not defaults
+const SYNC_EXAMPLE_OPTS = {
+  ...SYNC_DEFAULT_OPTS,
   defaultValue: '/home/user/exampleDir/',
   inside: true,
   filenameFilter: EXAMPLE_FILTER,
   pathFilter: EXAMPLE_FILTER,
+}
+
+const ASYNC_EXAMPLE_OPTS = {
+  ...SYNC_EXAMPLE_OPTS,
   exist: false,
   dir: true,
   canWrite: true,
   canExecute: true,
   statFilter: () => true,
 }
+
+const EXAMPLE_OPTS = { sync: SYNC_EXAMPLE_OPTS, async: ASYNC_EXAMPLE_OPTS }
 
 // This is the default `condition` from `jest-validate`, but with customize
 // handling for `*Filter` options, because `jest-validate` does not handle
