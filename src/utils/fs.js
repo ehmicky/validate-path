f0b5fcd5889447da0b9209793fe3635c54cf1dfa
ehmicky <ehmicky@gmail.com>
2019-02-19 10:00:00 +0100

    Remove util.promisify

--- src/utils/fs.js
+++ src/utils/fs.js
@@ -1,8 +1,7 @@
 'use strict'
 
+const { promisify } = require('util')
 const { stat, realpath } = require('fs')
-
-const promisify = require('util.promisify')
 
 const pStat = promisify(stat)
 const pRealpath = promisify(realpath)
